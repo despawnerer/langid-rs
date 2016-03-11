354f4961704c23eb5ac711254e20fa3a7a4fd38a
Aleksei Voronov <despawn@gmail.com>
2016-03-11 21:13:37 +0300

    Use the standard min_by_key method
    …instead of custom, shitty implementation

--- src/classifier.rs
+++ src/classifier.rs
@@ -20,27 +20,10 @@ impl Classifier {
 
     pub fn classify(&self, text: &str) -> &String {
         let model = Model::build_from_text(text);
-        let (name, matched_model) = find_min_by_key(
-            self.models.iter(),
-            |&(name, other_model)| model.compare(other_model)
-        ).unwrap();
+        let (name, matched_model) = self.models
+            .iter()
+            .min_by_key(|&(name, other_model)| model.compare(other_model))
+            .unwrap();
         name
     }
-}
-
-
-fn find_min_by_key<T, I, F>(iterable: I, key: F) -> Option<T>
-    where I : IntoIterator<Item=T>,
-          F : Fn(&T) -> usize {
-
-    let mut min_value = None;
-    let mut min_item = None;
-    for item in iterable {
-        let value = key(&item);
-        if min_value == None || value < min_value.unwrap() {
-            min_value = Some(value);
-            min_item = Some(item);
-        }
-    }
-    min_item
 }
