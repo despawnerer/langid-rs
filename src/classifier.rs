8995403eb7db7dac7a708a5b9b28a9e52aa903bd
Aleksei Voronov <despawn@gmail.com>
2016-03-12 15:41:31 +0300

    Turn the model list in Classifier into a vector
    There’s really no need in a hashmap here, we’re matching by iterating
    anyway

--- src/classifier.rs
+++ src/classifier.rs
@@ -1,28 +1,26 @@
-use std::collections::HashMap;
-
 use model::Model;
 
 
 pub struct Classifier {
-    models: HashMap<String, Model>
+    models: Vec<(String, Model)>
 }
 
 
 impl Classifier {
     pub fn new() -> Classifier {
-        Classifier { models: HashMap::new() }
+        Classifier { models: Vec::new() }
     }
 
     pub fn train(&mut self, text: &str, name: &str) {
         let model = Model::build_from_text(text);
-        self.models.insert(name.to_string(), model);
+        self.models.push((name.to_string(), model));
     }
 
     pub fn classify(&self, text: &str) -> &String {
         let model = Model::build_from_text(text);
-        let (name, _) = self.models
+        let &(ref name, _) = self.models
             .iter()
-            .min_by_key(|&(_, other_model)| model.compare(other_model))
+            .min_by_key(|&&(_, ref other_model)| model.compare(other_model))
             .unwrap();
         name
     }
