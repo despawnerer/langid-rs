c3b1158bd39f27e9b95f5679f3b45b300e4cdf09
Aleksei Voronov <despawn@gmail.com>
2016-03-12 16:05:14 +0300

    Simplify the ngrams-generating code considerably

--- src/ngrams.rs
+++ src/ngrams.rs
@@ -1,60 +1,39 @@
 use std;
 
 
-pub fn ngrams(text: &str, length: usize) -> NGrams {
-    NGrams {
+pub fn ngrams(text: &str, length: usize) -> NGramIterator {
+    NGramIterator {
         length: length,
         chars: text.chars(),
-        last_ngram: String::with_capacity(length*2),
+        last_ngram: String::new(),
     }
 }
 
-pub struct NGrams<'a> {
+pub struct NGramIterator<'a> {
     length: usize,
     chars: std::str::Chars<'a>,
     last_ngram: String,
 }
 
 
-impl<'a> Iterator for NGrams<'a> {
+impl<'a> Iterator for NGramIterator<'a> {
     type Item = String;
 
     fn next(&mut self) -> Option<String> {
-        if self.last_ngram.len() < self.length {
-            while self.last_ngram.len() < self.length {
-                match self.chars.next() {
-                    Some(character) => {
-                        self.last_ngram.push(character);
-                    },
-                    None => {
-                        return None;
-                    },
-                };
-            }
-            return Some(self.last_ngram.clone());
+        let mut ngram: String = self.last_ngram.chars().skip(1).collect();
+
+        while ngram.len() < self.length {
+            match self.chars.next() {
+                Some(character) => {
+                    ngram.push(character);
+                },
+                None => {
+                    return None;
+                },
+            };
         }
 
-        match self.chars.next() {
-            Some(character) => {
-                let ngram = self.build_new_ngram(character);
-                self.last_ngram = ngram.clone();
-                Some(ngram)
-            },
-            None => None,
-        }
-    }
-}
-
-impl<'a> NGrams<'a> {
-    fn build_new_ngram(&mut self, new_character: char) -> String {
-        let mut ngram = String::with_capacity(self.length*2);
-        let current_chars = self.last_ngram.chars().skip(1);
-
-        for character in current_chars {
-            ngram.push(character);
-        }
-        ngram.push(new_character);
-
-        ngram
+        self.last_ngram = ngram.clone();
+        return Some(ngram);
     }
 }
