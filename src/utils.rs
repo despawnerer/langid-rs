fd0085c922501573ebf8528c3e3b346b1e4183c9
Aleksei Voronov <despawn@gmail.com>
2016-03-22 19:37:05 +0300

    Add a forgotten read_file function to utils

--- src/utils.rs
+++ src/utils.rs
@@ -10,6 +10,13 @@ pub fn write_file<P: AsRef<Path>>(path: P, contents: &Vec<u8>) -> io::Result<()>
 }
 
 
+pub fn read_file<P: AsRef<Path>>(path: P, buf: &mut Vec<u8>) -> io::Result<usize> {
+    let mut file = try!(File::open(path));
+    let bytes = try!(file.read_to_end(buf));
+    Ok(bytes)
+}
+
+
 pub fn read_file_to_string<P: AsRef<Path>>(path: P, buf: &mut String) -> io::Result<usize> {
     let mut file = try!(File::open(path));
     let bytes = try!(file.read_to_string(buf));
